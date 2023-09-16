dba59580916a9563c0ffc022ea24d9f933c696a7
timothek2112 <nikzav6080@gmail.com>
2023-09-16 10:15:52 +0700

    feature: started car movement

--- Assets/Objects/Car/Block/Scripts/StateMachine/States/Connected.cs
+++ Assets/Objects/Car/Block/Scripts/StateMachine/States/Connected.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
 public class Connected : IBlockState
 {
+    Rigidbody2D body;
     public Connected(Block block) : base(block)
     {
     }
 
     public override void Enter()
     {
+        body = block.GetComponent<Rigidbody2D>(); 
         block.transform.SetParent(GameObject.FindGameObjectWithTag("CarBlocks").transform);
-        block.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        block.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
     }
 
     public override void Process(float dTime)
     {
-
+        body.velocity = new Vector2(0,0);
     }
 
     public override void Exit()
--- Assets/Objects/Car/Scripts/Movement.cs
+++ Assets/Objects/Car/Scripts/Movement.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    void Start()
+    public float weight = 10;
+    public float speed = 2;
+    public float accelerationFactor = 0.5f;
+    public float turnSpeed = 2;
+    public float stoppingFactor = 0.95f;
+    Rigidbody2D _body;
+    void Awake()
     {
-        
+        _body = GetComponent<Rigidbody2D>();
     }
+
+    
 
     void Update()
     {
-
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(_body.velocity, transform.up);
+        _body.velocity = forwardVelocity * stoppingFactor;
+        _body.AddForce(transform.up * speed * vertical * Time.deltaTime * accelerationFactor, ForceMode2D.Force);
+        _body.MoveRotation(_body.rotation - horizontal *  Time.deltaTime * turnSpeed);
     }
 }
